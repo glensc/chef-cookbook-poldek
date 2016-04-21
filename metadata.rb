00c81dd4d522a7368c5167775469171fae598f75
Elan Ruusamäe <glen@delfi.ee>
2016-04-21 15:13:14 +0300

    initial recipe, configuring cachedir

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,6 @@ name             'poldek'
 maintainer       'Elan Ruusamäe'
 maintainer_email 'glen@delfi.ee'
 license          'Apache 2.0'
-description      'Provides CVS SCM Resource'
+description      'Mange poldek config and repositories'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-
